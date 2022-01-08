bb3759709f1bd8d01b51fce1c2f2ff53ad5aec55
Kunal Panigrahi <75083810+Kunal027@users.noreply.github.com>
2022-01-08 19:06:50 +0530

    Encapsulation  practice
    Encapsulation for the Car and Athlete App

---
+++ first/src/Intermediate/Encaptulation/Practice/BikeApp.java
@@ -0,0 +1,14 @@
+package Intermediate.Encaptulation.Practice;
+
+public class BikeApp {
+    public static void main(String[] args) {
+
+        YamahaFZS fzs = new YamahaFZS((String) "Yamaha", (short) 153, (short) 2,20,43);
+        YamahaFZS apache = new YamahaFZS((String) "TVS", (short) 310, (short) 2,
+                20,  43);
+
+        fzs.getDescription();
+        apache.getDescription();
+
+    }
+}
